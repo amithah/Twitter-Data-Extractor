eb8d2577b1788db65587804b39f7747d50bad2a2
amithah <amithah.nithin@gmail.com>
2021-10-14 14:36:17 +0400

    added default user

--- myaccount/views.py
+++ myaccount/views.py
@@ -35,7 +35,7 @@ def user_login(request):
                 messages.error(request, 'Invalid username or password')
                 return redirect('myaccount:login')
     else:
-        form = LoginForm
+        form = LoginForm(initial={'username':'test_user','password':'1234@abc'})
     return render(request, 'myaccount/login.html', {'form': form})
 
 
